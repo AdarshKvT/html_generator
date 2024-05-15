b81a28a6a58bb132c8261cd0dc0707675f882956
adarsh_kvt <48561692+AdarshKvT@users.noreply.github.com>
2024-05-16 01:04:18 +0530

    fixed report title multiple occurrence

--- report_generator.py
+++ report_generator.py
@@ -1,17 +1,35 @@
-def write_content(argument, file):
-    print("argument: " +argument)
+report_title = "Report: ACC TEST REPORT"
+file_path=r'C:\Python_Course\Pycharm\Pycharm\Script\Py_Canoe_auto\pythonProject1\Reports\report.html'
+
+def write_content(argument):
+
+    flag = check_report_title()
     input_string_lower = argument.lower()
-    # file = open(r'D:\services-workspace\python-scripts\git\html_generator\ACC_Reports copy.html', "w")
 
-    if "report" in input_string_lower:
-        file.writelines("<h1 style='color: red;text-align: center;background: lightgrey; border-bottom: 2px solid black;border-radius: 8px;'> {} </h1>" .format(argument))
+    with open(file_path, "a") as file:
+     
+        if flag == False:
+            file.write("<h1 style='color: red;text-align: center;background: lightgrey; border-bottom: 2px solid black;border-radius: 8px;'> {} </h1>" .format(report_title))
+            flag = True
 
-    if "pass" in input_string_lower:
-        file.writelines("<h2 style='color:black;background: green'>{}</h2>".format(argument))
-    elif "fail" in input_string_lower:
-        file.writelines("<h2 style='color:black;background: red'>{}</h2>".format(argument))
-    else:
-        file.writelines("<h2 style='color:black;background: lightgrey'> {} </h2>".format(argument))
+        if "test case" in input_string_lower:
+            file.write("<h2 style='color:black;background: yellow'> {} </h2>".format(argument))
 
-    print(f"HTML report generated successfully: {file}")
+        elif "pass" in input_string_lower:
+            file.write("<h3 style='color:black;background: green'>{}</h3>".format(argument))
+
+        elif "fail" in input_string_lower:
+            file.write("<h3 style='color:black;background: red'>{}</h3>".format(argument))
+
+        else:
+            file.write("<h3 style='color:black;background: lightgrey'> {} </h3>".format(argument))
+
     return file
+
+def check_report_title():
+    with open(file_path, "r") as file:
+        for line in file:
+            if "<h1" in line and report_title in line:
+                return True   
+            break                                                     
+    return False
